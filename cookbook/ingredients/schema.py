1c227423dd1721191a5db4915d75716233357258
monish <me.monish@outlook.com>
2019-12-09 01:40:02 +0530

    pagination

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from ingredients.models import Category, Ingredient
-
+from django.db.models import Q
 
 class CategoryType(DjangoObjectType):
     class Meta:
@@ -15,12 +15,29 @@ class IngredientType(DjangoObjectType):
 
 class Query(object):
     all_categories = graphene.List(CategoryType)
-    all_ingredients = graphene.List(IngredientType)
+    all_ingredients = graphene.List(IngredientType,
+    							search=graphene.String(),
+						        first=graphene.Int(),
+						        skip=graphene.Int(),
+    							)
 
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
 
-    def resolve_all_ingredients(self, info, **kwargs):
+    def resolve_all_ingredients(self, info, search=None, first=None, skip=None,**kwargs):
         # We can easily optimize query count in the resolve method
-        return Ingredient.objects.select_related('category').all()
+        ing = Ingredient.objects.select_related('category').all()
+        if search:
+        	filter = (
+        		Q(name__icontains=search)|
+        		Q(notes__contains=search)
+        		)
+        	ing = ing.filter(filter)
+
+        if skip:
+        	ing = ing[skip:]
+        if first:
+        	ing = ing[:first]
+
+        return ing
 
